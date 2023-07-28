805abd06e17fce1b1998ef4f37c0458647ee14a9
Michael Haji <michaelh1277@gmail.com>
2023-07-28 01:59:13 -0700

    update app.js

--- app.js
+++ app.js
@@ -1,19 +1,24 @@
+// Initialize the scores for the home and away teams.
 let homeTeamScore = 0
 let awayTeamScore = 0
 
+// Get references to the HTML elements that display the scores.
 let homeTeamScoreEl = document.getElementById('homeTeamScoreEl')
 let awayTeamScoreEl = document.getElementById('awayTeamScoreEl')
 
+// Function to play a sound effect with optional volume adjustment (0 to 1).
 function playSoundEffect (audioId, volume = 1) {
   const audioElement = document.getElementById(audioId)
   if (audioElement) {
-    audioElement.volume = volume // Volume adjustment (0 to 1)
-    audioElement.play()
+    audioElement.volume = volume // Set the volume level.
+    audioElement.play() // Play the audio.
   }
 }
 
+// Function to add points to a team and update the score display.
 function addPointsToTeam (team, points) {
   let scoreElement
+  // Determine which team's score to update based on the 'team' parameter.
   if (team === 'home') {
     homeTeamScore += points
     scoreElement = homeTeamScoreEl
@@ -22,20 +27,23 @@ function addPointsToTeam (team, points) {
     scoreElement = awayTeamScoreEl
   }
 
+  // Update the score display for the corresponding team.
   if (scoreElement) {
     scoreElement.textContent = team === 'home' ? homeTeamScore : awayTeamScore
-    updateFontSize(scoreElement)
+    updateFontSize(scoreElement) // Adjust the font size based on the score length.
   }
 
+  // Play the appropriate sound effect based on the points scored, at 10% volume.
   if (points === 1) {
-    playSoundEffect('audio1', 0.1) // Adjust volume level (0.1 means 10% volume)
+    playSoundEffect('audio1', 0.1)
   } else if (points === 2) {
-    playSoundEffect('audio2', 0.1) // Adjust volume level (0.1 means 10% volume)
+    playSoundEffect('audio2', 0.1)
   } else if (points === 3) {
-    playSoundEffect('audio3', 0.1) // Adjust volume level (0.1 means 10% volume)
+    playSoundEffect('audio3', 0.1)
   }
 }
 
+// Function to reset both teams' scores and update the display.
 function resetScores () {
   homeTeamScore = 0
   awayTeamScore = 0
@@ -44,22 +52,27 @@ function resetScores () {
   updateFontSize(homeTeamScoreEl)
   updateFontSize(awayTeamScoreEl)
 
-  playSoundEffect('audio4', 0.1) // Adjust volume level (0.1 means 10% volume)
+  // Play the 'game-over' sound effect at 10% volume.
+  playSoundEffect('audio4', 0.1)
 }
 
+// Function to update the font size of the score display based on its content length.
 function updateFontSize (scoreEl) {
   const containerWidth = scoreEl.clientWidth
   const textWidth = scoreEl.scrollWidth
   const scoreText = scoreEl.textContent
 
+  // Adjust the font size to fit the score within the container, scaled by 1.4 for aesthetics.
   if (textWidth > containerWidth) {
     const fontSize = (containerWidth / scoreText.length) * 1.4
     scoreEl.style.fontSize = `${fontSize}px`
   } else {
+    // If the score fits within the container, set a default font size of 60px.
     scoreEl.style.fontSize = '60px'
   }
 }
 
+// Wait for the DOM to load before initializing the score displays.
 document.addEventListener('DOMContentLoaded', () => {
   updateFontSize(homeTeamScoreEl)
   updateFontSize(awayTeamScoreEl)
